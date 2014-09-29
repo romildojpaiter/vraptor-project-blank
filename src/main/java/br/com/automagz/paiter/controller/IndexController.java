371cc6fc4a60d7495d1f1c25cc73dadcf2acc7f3
Romildo Jozué Paiter <romildopaiter@gmail.com>
2014-09-29 00:33:01 -0400

    Envio do projeto vraptor-project-blank

---
+++ src/main/java/br/com/automagz/paiter/controller/IndexController.java
@@ -0,0 +1,15 @@
+package br.com.automagz.paiter.controller;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class IndexController {
+		
+	public void index(){
+		
+	}
+
+	public void ola(){
+		System.out.println("Ola Mundo!!!");
+	}
+}
